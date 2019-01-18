e6f400ae7b14729884f2863778dbf121aa810dd3
Frédéric Praca <frederic.praca@free.fr>
2019-01-18 23:32:55 +0100

    - Using PolyORB Corba utils to simplify the code

--- Ada/server/src/server.adb
+++ Ada/server/src/server.adb
@@ -4,14 +4,14 @@ with Ada.Text_IO; use Ada.Text_IO;
 with CORBA.Impl;
 with CORBA.Object;
 with CORBA.ORB;
- 
-with PortableServer.POA.Helper;
-with PortableServer.POAManager;
- 
+
 with CorbaCBSG.CBSG.Impl;
  
 with PolyORB.CORBA_P.CORBALOC;
 with PolyORB.CORBA_P.Naming_Tools;
+with PolyORB.CORBA_P.Server_Tools;
+
+with PortableServer;
 
 -- Allow to specify how PolyORB should work
 with PolyORB.Setup.Thread_Per_Request_Server;
@@ -19,7 +19,7 @@ pragma Warnings (Off, PolyORB.Setup.Thread_Per_Request_Server);
  
 procedure Server is
 begin
- 
+   
    declare
       -- Allow to get the parameters according to the CORBA Standard
       -- For example, InitialRef
@@ -28,56 +28,45 @@ begin
    begin
       -- Init of our bus named ORB
       CORBA.ORB.Init (CORBA.ORB.To_CORBA_String ("ORB"), Argv);
- 
+
+      --  Then perform all the stuff related to RootPOA
+      PolyORB.CORBA_P.Server_Tools.Activate_Server;
+   
       declare
-	 --  The PortableObjectAdapter is the place we "store" our objects
-         Root_POA : PortableServer.POA.Local_Ref;
- 
-	 --  We declare a reference to our distributed object
+      
+         --  We declare a reference to our distributed object
          Ref : CORBA.Object.Ref;
  
-	 --  And its implementation
+         --  And its implementation
          Obj : constant CORBA.Impl.Object_Ptr := new CorbaCBSG.CBSG.Impl.Object;
 	 
-	 --  And finally its name
-	 Obj_Name : constant String := "/cbsg/generator";
+         --  And finally its name
+         Obj_Name : constant String := "/cbsg/generator";
 	 
       begin
+         --  We create a reference on our object (the servant) to expose it to the outside world
+         PolyORB.CORBA_P.Server_Tools.Initiate_Servant (S => PortableServer.Servant (Obj),
+                                                        R => Ref);
  
-	 --  We get the root POA of our bus
-	 --  It's a CORBA interface, so note the use of CORBA String instead of Ada ones
-	 --  We then resolve it to an object reference
-         Root_POA := PortableServer.POA.Helper.To_Local_Ref
-           (CORBA.ORB.Resolve_Initial_References
-	      (CORBA.ORB.To_CORBA_String ("RootPOA")));
- 
-	 --  We start our POA (in fact, the top-level one)
-         PortableServer.POAManager.Activate
-           (PortableServer.POA.Get_The_POAManager (Root_POA));
- 
-	 --  We create a reference on our object (the servant) to expose it to the outside world
-         Ref := PortableServer.POA.Servant_To_Reference
-           (Root_POA, PortableServer.Servant (Obj));
- 
-	 --  And we display its address, the IOR.
+         --  And we display its address, the IOR.
          Put_Line
            ("'"
-	      & CORBA.To_Standard_String (CORBA.Object.Object_To_String (Ref))
-	      & "'");
+            & CORBA.To_Standard_String (CORBA.Object.Object_To_String (Ref))
+            & "'");
          New_Line;
  
          --  And its shorter version, the corbaloc
-	 --  unfortunately, corbaloc is not supported by every ORB implementation
+         --  unfortunately, corbaloc is not supported by every ORB implementation
          Put_Line
            ("'"
-	      & CORBA.To_Standard_String
-	      (PolyORB.CORBA_P.CORBALOC.Object_To_Corbaloc (Ref))
-	      & "'");
+            & CORBA.To_Standard_String
+              (PolyORB.CORBA_P.CORBALOC.Object_To_Corbaloc (Ref))
+            & "'");
 	 
-	 --  Register it to Naming service
-	 PolyORB.CORBA_P.Naming_Tools.Register (Obj_Name, Ref, Rebind => True);
+         --  Register it to Naming service
+         PolyORB.CORBA_P.Naming_Tools.Register (Obj_Name, Ref, Rebind => True);
 	 
-	 Put_Line ("Generator rebound");
+         Put_Line ("Generator rebound");
 	      
          --  Launch the server. CORBA.ORB.Run is supposed to never return,
          --  print a message if it does.
@@ -88,7 +77,7 @@ begin
       end;
    end;
 exception
-   --  Of course, we display a message in case of exception
+      --  Of course, we display a message in case of exception
    when E : others =>
       Put_Line
         ("CBSG server raised " & Ada.Exceptions.Exception_Information (E));
